70e7d4732addd4e412d3fa6ceda6200fbfa99074
braulio0790 <braulio90.lds@gmail.com>
2025-01-26 00:35:35 -0600

    Abstraction Learning Activity Week3

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Create an object and initialize
+        Job displayJob = new Job();
+        displayJob.display();
+        Resume displayResume = new Resume();
+        displayResume.display();
     }
 }
