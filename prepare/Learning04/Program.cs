5958356b3ea11b6dd6b29fa11eda5a3ade86d173
braulio0790 <braulio90.lds@gmail.com>
2025-02-26 03:31:27 -0600

    Prepare Learning Activity Week7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        //Create object for MathAssignment, assing the values, and display them
+        MathAssignment math = new MathAssignment("Braulio", "Math","Chapter 1", "Exercise 1\n" );
+        Console.WriteLine(math.GetHomeworkList());
+
+        //Create object for WritingAssignment, assing the values, and display them
+        WritingAssignment writing = new WritingAssignment("Braulio", "Literature","SalemsLot");
+        Console.WriteLine(writing.GetWritingInformation());        
     }
 }
